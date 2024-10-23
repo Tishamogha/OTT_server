d6db4561ada941f2fe2a2d56225cd048a224523a
Tisha Mogha <86661488+Tishamogha@users.noreply.github.com>
2024-10-22 22:00:48 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Home from './pages/Home/Home'
-import {Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
+import Movie from './pages/Movie/Movie'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element ={<Login/>}/>
+        <Route path='/player/:id' element={<Player />}/>
+        <Route path="/movies" element={<Movie />} />
       </Routes>
     </div>
   )
